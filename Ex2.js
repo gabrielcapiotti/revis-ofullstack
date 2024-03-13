79cd766d3b74e7e242e6df480dda2f5e4608efcc
gabrielcapiotti <132498423+gabrielcapiotti@users.noreply.github.com>
2024-03-13 16:38:23 -0300

    Create Ex2.js

---
+++ Ex2.js
@@ -0,0 +1,2 @@
+/*2. Imprima no console usando node as seguintes informações: "Meu nome é X, tenho Y anos e meus hobbies são Z1, Z2 e Z3."*/
+console.log("Meu nome é Gabriel Capiotti, tenho 23 anos e meus hobbies são escrita criativa, video-games e literatura.");
